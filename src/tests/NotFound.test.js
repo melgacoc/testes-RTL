6906c4b7ef298888b191797d35c4f7ae63037e25
Claudio Melgaco <claudio19a@hotmail.com>
2022-09-02 17:56:45 -0300

    req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,5 +1,21 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
 describe('Testa o componente Not Found', () => {
   it('Testa se a págna contém um heading h2', () => {
+    renderWithRouter(<NotFound />);
 
+    const invalid = screen.getByRole('heading',
+      { name: /Page requested not found/i, level: 2 });
+    expect(invalid).toBeInTheDocument();
+  });
+
+  it('Testa se a página reenderiza imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const image = screen.getByRole('img', { name: /Pikachu crying/i });
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
   });
 });
